9a8795e43ffecc423ed2ec39087654e5670e2ca3
John Doe <nfhyev11@gmail.com>
2023-09-18 21:27:14 +0500

    - rotating cube

--- ClientApp/PortfolioProject/src/app/client/applications/schedule/schedule.component.ts
+++ ClientApp/PortfolioProject/src/app/client/applications/schedule/schedule.component.ts
@@ -13,6 +13,10 @@ export class ScheduleComponent extends BaseComponent {
     mobileList: string[] = []
     adminList: string[] = []
     cssVariablesService = inject(CssVariablesService)
+    x = 0
+    y = 0
+    changeX = 2
+    changeY = 2
     override ngOnInit(): void {
         super.ngOnInit()
 
@@ -29,6 +33,18 @@ export class ScheduleComponent extends BaseComponent {
         setInterval(() => {
             this.cssVariablesService.setVariable("--lamp-color", "#00000000")
         }, 100)
+        setInterval(()=>{
+            let cube = document.querySelector('.cube')
+            if(cube == null){
+                return
+            }
+            this.x += this.changeX
+            this.y += this.changeY
+        },200)
+        setInterval(()=>{
+            this.changeX = (Math.random() - 0.5) * 4
+            this.changeY = (Math.random() - 0.5) * 4
+        },5000)
     }
     beginningText = "The project idea was born under mundane circumstances - it was third year and several competitions behind my back, but none of the projects were truly practical, useful to someone. So the answer to the question \"What could I do with my skills that would actually be worthwhile?\" was electronic schedule. The first reason is practical - my weak eyesight always made it harder to read the paper schedule. The second one was irony of whole situation - I studied as a software engineer, but my university didn't have a simple electronic schedule. Truly ironic. And slightly embarrassing."
     resultsText = `The university agreed to a test run of the system, but ultimately was not interested in supporting the application long term, so the production time was limited to a year in my group and half a year for the whole university`
