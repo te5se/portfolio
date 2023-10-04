d19010906d645c852080b9e9910af39b9869ff3c
John Doe <nfhyev11@gmail.com>
2023-10-04 20:39:09 +0500

    - starting work on pws03

--- ClientApp/PortfolioProject/src/app/client/applications/pws03/pws03.component.ts
+++ ClientApp/PortfolioProject/src/app/client/applications/pws03/pws03.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-pws03',
-  templateUrl: './pws03.component.html',
-  styleUrls: ['./pws03.component.css']
+    selector: 'app-pws03',
+    templateUrl: './pws03.component.html',
+    styleUrls: ['./pws03.component.css']
 })
 export class Pws03Component implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    leftWidth : string = '50%'
+
+    ngOnInit(): void {
+        this.setupMouseMoveListener()
+    }
+    setupMouseMoveListener() {
+       
+        
+    }
+    @HostListener('document:mousemove', ['$event'])
+    onMouseMove(e : MouseEvent) {
+        this.leftWidth = `${e.clientX / window.innerWidth * 100}%`;
+    }
+
+    @HostListener('document:touchmove', ['$event'])
+    onTouchMove(e : any) {
+        this.leftWidth = `${e.touches[0].clientX / window.innerWidth * 100}%`;
+    }
 
 }
--- ClientApp/PortfolioProject/src/app/client/client-routing.module.ts
+++ ClientApp/PortfolioProject/src/app/client/client-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientStartingPageComponent } from './client-starting-page/client-starting-page.component';
 import { ScheduleComponent } from './applications/schedule/schedule.component';
+import { Pws03Component } from './applications/pws03/pws03.component';
 
 const routes: Routes = [
     { path: 'projects', component: ClientStartingPageComponent},
-    { path: 'schedule', component: ScheduleComponent}
+    { path: 'schedule', component: ScheduleComponent},
+    { path: 'pws03', component: Pws03Component}
 ];
 
 @NgModule({
