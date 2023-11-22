8f143c8ebcac286e0d3c3927dc9d0dd243efc744
te5se <133968742+te5se@users.noreply.github.com>
2023-11-22 18:20:25 +0500

    first and start of second sections

--- ClientApp/PortfolioProject/src/app/client/applications/pad77/pad77.component.ts
+++ ClientApp/PortfolioProject/src/app/client/applications/pad77/pad77.component.ts
@@ -1,15 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { BaseComponent } from 'src/app/components/base/base.component';
 
 @Component({
   selector: 'app-pad77',
   templateUrl: './pad77.component.html',
   styleUrls: ['./pad77.component.css']
 })
-export class Pad77Component implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class Pad77Component extends BaseComponent{
 
 }
