ae0cc4d522e6d6e2521de87c385cf3366a504842
te5se <133968742+te5se@users.noreply.github.com>
2023-12-17 19:50:49 +0500

    - Rejection probability - Disabled button while in progress - status string

--- ClientApp/PortfolioProject/src/app/client/applications/pad77/customizable-routing/customizable-routing.component.ts
+++ ClientApp/PortfolioProject/src/app/client/applications/pad77/customizable-routing/customizable-routing.component.ts
@@ -1,5 +1,6 @@
 import { APP_BASE_HREF } from '@angular/common';
-import { Component, ElementRef, OnInit, inject } from '@angular/core';
+import { Component, ElementRef, OnInit, Output, inject } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { TimeHelper } from 'src/app/helpers/timehelper';
 
 @Component({
@@ -9,84 +10,164 @@ import { TimeHelper } from 'src/app/helpers/timehelper';
 })
 export class CustomizableRoutingComponent implements OnInit {
 
+  @Output() status = new BehaviorSubject<string>("default status")
+  @Output() isInProgress = new BehaviorSubject<boolean>(false)
+
   baseHref = inject(APP_BASE_HREF)
   personFilled = `url(${this.baseHref}assets/person_fill.png)`
 
-  offsets : ItemOffset[] = []
+  offsets: ItemOffset[] = []
 
   movingDocY = 0
   movingDocX = 0
   showDocument = false
   duration = 500
   documentIcon = "contract"
+  animateAccept = false
+  animateReject = false
+
+  numberOfCoordinators = 3
+
+  passingRate = 0.2
 
   constructor() { }
   ngOnInit(): void {
 
-    setTimeout(()=>{
+    setTimeout(() => {
       this.setupOffsets()
     })
-    setInterval(()=>{
+    setInterval(() => {
       this.setupOffsets()
-    },100)
+    }, 100)
   }
 
-  async runDocument(){
+  async runDocument() {
+    this.isInProgress.next(true)
+
     this.duration = 0
     this.moveDocumentTo("initiator1")
+    this.status.next(`Create document`)
     await TimeHelper.delay(0)
     this.showDocument = true
     this.duration = 500
-    this.moveDocumentTo("coordinator1")
-    await TimeHelper.delay(500)
-    this.moveDocumentTo("coordinator2")
-    await TimeHelper.delay(500)
-    this.moveDocumentTo("coordinator3")
-    await TimeHelper.delay(500)
+
+    let result = await this.runCoordinatorStage()
+    if (result == false) {
+      this.showDocument = false
+      return
+    }
+
     this.moveDocumentTo("supervisor1")
-    await TimeHelper.delay(500)
+    this.status.next(`Document vised`)
+    await TimeHelper.delay(800)
     this.moveDocumentTo("initiator2")
-    await TimeHelper.delay(500)
+    this.status.next(`Created task by the document`)
+    await TimeHelper.delay(800)
     this.moveDocumentTo("supervisor2")
+    this.status.next(`Report sent to the supervisor`)
+    await TimeHelper.delay(800)
+    this.animateAccept = true
     await TimeHelper.delay(500)
+    this.animateAccept = false
+    this.status.next(`Report accepted`)
+
+    this.showDocument = false
+    this.isInProgress.next(false)
   }
-  changeDocumentIcon(){
-    let icons = ["contract","description", "content_paste", "assignment_turned_in", "text_snippet", "note", "news", "two_pager"]
-    let randomResult = Math.floor(Math.random()*icons.length)
+  // return value means SHOULD_CONTINUE
+  async runCoordinatorStage(): Promise<boolean> {
+    let currentStage = 0
+    for (let i = 0; i < this.numberOfCoordinators; i++) {
+      let shouldAccept = Math.random() - this.passingRate > 0
+      let id = "coordinator" + i
+      this.moveDocumentTo(id)
+      await TimeHelper.delay(500)
+      this.animateAccept = shouldAccept
+      this.animateReject = !shouldAccept
+      await TimeHelper.delay(500)
+      this.animateAccept = false
+      this.animateReject = false
+
+      // move to start
+      if (shouldAccept == false) {
+        let firstItem = this.offsets.find((offset) => {
+          return offset.id = "initiator1"
+        })
+        let lowestItem = this.offsets.find((offset) => {
+          return offset.id = "coordinator" + this.numberOfCoordinators
+        })
+        if (firstItem?.id == null || lowestItem?.id == null) {
+          return false
+        }
+        this.status.next(`Rejected by ${this.getWordForOrder(i + 1)} coordinator`)
+        this.movingDocY = lowestItem.top! + 200
+        await TimeHelper.delay(500)
+        this.movingDocX = firstItem.left!
+        await TimeHelper.delay(500)
+        this.movingDocY = firstItem.top!
+        await TimeHelper.delay(500)
+        return false
+      } else {
+        this.status.next(`Accepted by ${this.getWordForOrder(i + 1)} coordinator`)
+      }
+    }
+    return true
+  }
+  getWordForOrder(order: number) {
+    if (order == 1) {
+      return "first"
+    }
+    if (order == 2) {
+      return "second"
+    }
+    if (order == 3) {
+      return "third"
+    }
+    if (order == 4) {
+      return "fourth"
+    }
+    if (order == 5) {
+      return "fifth"
+    }
+    return ""
+  }
+  changeDocumentIcon() {
+    let icons = ["contract", "description", "content_paste", "assignment_turned_in", "text_snippet", "note", "news", "two_pager"]
+    let randomResult = Math.floor(Math.random() * icons.length)
 
     this.documentIcon = icons[randomResult]
   }
-  setupOffsets(){
+  setupOffsets() {
     this.offsets = []
 
     let secondSection = document.getElementById("second-section")
-        
-    if (secondSection == null){
+
+    if (secondSection == null) {
       return
     }
 
     this.offsets.push(this.getOffsetItem("initiator1", secondSection))
-    this.offsets.push(this.getOffsetItem("coordinator1", secondSection))
-    this.offsets.push(this.getOffsetItem("coordinator2", secondSection))
-    this.offsets.push(this.getOffsetItem("coordinator3", secondSection))
+    for (let i = 0; i < this.numberOfCoordinators; i++) {
+      this.offsets.push(this.getOffsetItem("coordinator" + i.toString(), secondSection))
+    }
     this.offsets.push(this.getOffsetItem("supervisor1", secondSection))
     this.offsets.push(this.getOffsetItem("initiator2", secondSection))
     this.offsets.push(this.getOffsetItem("supervisor2", secondSection))
   }
-  getOffsetItem(id : string, relativeTo : HTMLElement) : ItemOffset{
+  getOffsetItem(id: string, relativeTo: HTMLElement): ItemOffset {
     let result = document.getElementById(id)
-    if(result == null){
+    if (result == null) {
       return {}
     }
     let offset = this.getOffset(result, relativeTo)
-    return{
-      id : id,
-      left : offset.left,
-      top : offset.top
+    return {
+      id: id,
+      left: offset.left,
+      top: offset.top
     }
   }
 
-  getOffset(el : HTMLElement, relativeTo : HTMLElement) {
+  getOffset(el: HTMLElement, relativeTo: HTMLElement) {
     const elRect = el.getBoundingClientRect();
     const relativeToRect = relativeTo.getBoundingClientRect();
     return {
@@ -94,12 +175,12 @@ export class CustomizableRoutingComponent implements OnInit {
       top: elRect.top - relativeToRect.top
     };
   }
-  moveDocumentTo(id : string){
-    let offset = this.offsets.find((offset)=>{
+  moveDocumentTo(id: string) {
+    let offset = this.offsets.find((offset) => {
       return offset.id == id
     })
 
-    if(offset?.left == null || offset?.top == null){
+    if (offset?.left == null || offset?.top == null) {
       return
     }
 
@@ -111,8 +192,8 @@ export class CustomizableRoutingComponent implements OnInit {
   }
 
 }
-export interface ItemOffset{
-  id? : string
-  left? : number
-  top? : number
+export interface ItemOffset {
+  id?: string
+  left?: number
+  top?: number
 }
--- ClientApp/PortfolioProject/src/app/client/applications/pad77/pad77.component.ts
+++ ClientApp/PortfolioProject/src/app/client/applications/pad77/pad77.component.ts
@@ -1,203 +1,226 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, ViewChild, inject } from '@angular/core';
 import { BaseComponent } from 'src/app/components/base/base.component';
 import { PAD77ParallaxItem } from './parallax-document/parallax-document.component';
 import { APP_BASE_HREF } from '@angular/common';
+import { CustomizableRoutingComponent } from './customizable-routing/customizable-routing.component';
+import { TimeHelper } from 'src/app/helpers/timehelper';
 
 @Component({
-  selector: 'app-pad77',
-  templateUrl: './pad77.component.html',
-  styleUrls: ['./pad77.component.css']
+    selector: 'app-pad77',
+    templateUrl: './pad77.component.html',
+    styleUrls: ['./pad77.component.css']
 })
 export class Pad77Component extends BaseComponent {
 
-  baseHref = inject(APP_BASE_HREF)
+    @ViewChild("customizableRouting") customizableRouting: CustomizableRoutingComponent | undefined
 
-  array30 = (new Array(30)).fill(0)
+    baseHref = inject(APP_BASE_HREF)
 
-  parallaxWrappers: ParallaxWrapper[] = []
+    array30 = (new Array(30)).fill(0)
 
-  constructor() {
-    super()
+    parallaxWrappers: ParallaxWrapper[] = []
 
-    this.createParallaxWrappers()
-  }
+    runDocumentCaption = "run the document"
+    isRoutingInProgress = false
 
-  createParallaxWrappers() {
-    this.parallaxWrappers.push({
-      leftPercent: 60,
-      parallaxSpeed: -0.3,
-      parallaxItem: {
-        badge: 5,
-        isQEC: true,
-        time: '18:45',
-        title: 'Приказ по МТР',
-        opacity: 0.3,
-        tndex: 0
-      },
-      scale: 1.5,
-      bottomMarginPX: 1600
-    })
-    this.parallaxWrappers.push({
-      leftPercent: 80,
-      parallaxSpeed: 0,
-      parallaxItem: {
-        badge: 2,
-        isQEC: false,
-        time: '08:10',
-        title: 'Входящий документ',
-        opacity: 1,
-        tndex: 1
-      },
-      scale: 1,
-      bottomMarginPX: 1000
-    })
-    this.parallaxWrappers.push({
-      leftPercent: 40,
-      parallaxSpeed: -0.1,
-      parallaxItem: {
-        badge: 2,
-        isQEC: false,
-        time: '16:48',
-        title: 'Разрешение на публикацию документа',
-        opacity: 1,
-        tndex: 2
-      },
-      scale: 1,
-      bottomMarginPX: 1000
-    })
-    this.parallaxWrappers.push({
-      leftPercent: 30,
-      parallaxSpeed: 0.5,
-      parallaxItem: {
-        badge: 2,
-        isQEC: true,
-        time: '12:12',
-        title: 'Приказ о выпуске продукции',
-        opacity: 0.7, 
-        tndex: 3
-      },
-      scale: 1.2,
-      bottomMarginPX: 500
-    })
-    this.parallaxWrappers.push({
-      leftPercent: 10,
-      parallaxSpeed: 1,
-      parallaxItem: {
-        badge: 6,
-        isQEC: false,
-        time: '20:18',
-        title: 'Предложение по улучшению производства',
-        opacity: 0.2,
-        tndex: 4
-      },
-      scale: 1.2,
-      bottomMarginPX: 0
-    })
-    this.parallaxWrappers.push({
-      leftPercent: 10,
-      parallaxSpeed: -0.2,
-      parallaxItem: {
-        badge: 6,
-        isQEC: false,
-        time: '20:18',
-        title: 'Положение об охране труда',
-        opacity: 0.2,
-        tndex: 5
-      },
-      scale: 1.5,
-      bottomMarginPX: 1600
-    })
-    this.parallaxWrappers.push({
-      leftPercent: 50,
-      parallaxSpeed: 1.5,
-      parallaxItem: {
-        badge: 1,
-        isQEC: false,
-        time: '12:20',
-        title: 'Разрешение на перенос отпуска',
-        opacity: 0.5,
-        tndex: 6
-      },
-      scale: 1.5,
-      bottomMarginPX: 0
-    })
-    this.parallaxWrappers.push({
-      leftPercent: 20,
-      parallaxSpeed: 0.3,
-      parallaxItem: {
-        badge: 2,
-        isQEC: false,
-        time: '15:20',
-        title: 'Заявка на списания со склада',
-        opacity: 0.8,
-        tndex: 7
-      },
-      scale: 1.5,
-      bottomMarginPX: 1000
-    })
-    this.parallaxWrappers.push({
-      leftPercent: 40,
-      parallaxSpeed: -0.2,
-      parallaxItem: {
-        badge: 1,
-        isQEC: false,
-        time: '11:20',
-        title: 'Приказ генерального директора',
-        opacity: 0.8,
-        tndex: 8
-      },
-      scale: 1,
-      bottomMarginPX: 1700
-    })
-    this.parallaxWrappers.push({
-      leftPercent: 70,
-      parallaxSpeed: 0.5,
-      parallaxItem: {
-        badge: 5,
-        isQEC: true,
-        time: '11:20',
-        title: 'План АСУП',
-        opacity: 0.6,
-        tndex: 9
-      },
-      scale: 1,
-      bottomMarginPX: 1000
-    })
-    this.parallaxWrappers.push({
-      leftPercent: 90,
-      parallaxSpeed: 1,
-      parallaxItem: {
-        badge: 5,
-        isQEC: true,
-        time: '11:20',
-        title: 'План по дебюрократизации',
-        opacity: 1,
-        tndex: 10
-      },
-      scale: 1,
-      bottomMarginPX: 0
-    })
-    this.parallaxWrappers.push({
-      leftPercent: 2,
-      parallaxSpeed: 1.5,
-      parallaxItem: {
-        badge: 5,
-        isQEC: true,
-        time: '11:20',
-        title: 'Отчёт по выполнению гос. заказа',
-        opacity: 1,
-        tndex: 11
-      },
-      scale: 1,
-      bottomMarginPX: 0
-    })
-  }
+    constructor() {
+        super()
+
+        this.createParallaxWrappers()
+    }
+
+    async runDocument() {
+        this.customizableRouting?.runDocument()
+        this.isRoutingInProgress = true
+        let i = 0
+        while (this.isRoutingInProgress) {
+            await TimeHelper.delay(100)
+            i++
+            this.runDocumentCaption = "in progress"
+            for (let c = 0; c < i % 3; c++) {
+                this.runDocumentCaption += '.'
+            }
+        }
+        this.runDocumentCaption = "run another document"
+    }
+
+
+    createParallaxWrappers() {
+        this.parallaxWrappers.push({
+            leftPercent: 60,
+            parallaxSpeed: -0.3,
+            parallaxItem: {
+                badge: 5,
+                isQEC: true,
+                time: '18:45',
+                title: 'Приказ по МТР',
+                opacity: 0.3,
+                tndex: 0
+            },
+            scale: 1.5,
+            bottomMarginPX: 1600
+        })
+        this.parallaxWrappers.push({
+            leftPercent: 80,
+            parallaxSpeed: 0,
+            parallaxItem: {
+                badge: 2,
+                isQEC: false,
+                time: '08:10',
+                title: 'Входящий документ',
+                opacity: 1,
+                tndex: 1
+            },
+            scale: 1,
+            bottomMarginPX: 1000
+        })
+        this.parallaxWrappers.push({
+            leftPercent: 40,
+            parallaxSpeed: -0.1,
+            parallaxItem: {
+                badge: 2,
+                isQEC: false,
+                time: '16:48',
+                title: 'Разрешение на публикацию документа',
+                opacity: 1,
+                tndex: 2
+            },
+            scale: 1,
+            bottomMarginPX: 1000
+        })
+        this.parallaxWrappers.push({
+            leftPercent: 30,
+            parallaxSpeed: 0.5,
+            parallaxItem: {
+                badge: 2,
+                isQEC: true,
+                time: '12:12',
+                title: 'Приказ о выпуске продукции',
+                opacity: 0.7,
+                tndex: 3
+            },
+            scale: 1.2,
+            bottomMarginPX: 500
+        })
+        this.parallaxWrappers.push({
+            leftPercent: 10,
+            parallaxSpeed: 1,
+            parallaxItem: {
+                badge: 6,
+                isQEC: false,
+                time: '20:18',
+                title: 'Предложение по улучшению производства',
+                opacity: 0.2,
+                tndex: 4
+            },
+            scale: 1.2,
+            bottomMarginPX: 0
+        })
+        this.parallaxWrappers.push({
+            leftPercent: 10,
+            parallaxSpeed: -0.2,
+            parallaxItem: {
+                badge: 6,
+                isQEC: false,
+                time: '20:18',
+                title: 'Положение об охране труда',
+                opacity: 0.2,
+                tndex: 5
+            },
+            scale: 1.5,
+            bottomMarginPX: 1600
+        })
+        this.parallaxWrappers.push({
+            leftPercent: 50,
+            parallaxSpeed: 1.5,
+            parallaxItem: {
+                badge: 1,
+                isQEC: false,
+                time: '12:20',
+                title: 'Разрешение на перенос отпуска',
+                opacity: 0.5,
+                tndex: 6
+            },
+            scale: 1.5,
+            bottomMarginPX: 0
+        })
+        this.parallaxWrappers.push({
+            leftPercent: 20,
+            parallaxSpeed: 0.3,
+            parallaxItem: {
+                badge: 2,
+                isQEC: false,
+                time: '15:20',
+                title: 'Заявка на списания со склада',
+                opacity: 0.8,
+                tndex: 7
+            },
+            scale: 1.5,
+            bottomMarginPX: 1000
+        })
+        this.parallaxWrappers.push({
+            leftPercent: 40,
+            parallaxSpeed: -0.2,
+            parallaxItem: {
+                badge: 1,
+                isQEC: false,
+                time: '11:20',
+                title: 'Приказ генерального директора',
+                opacity: 0.8,
+                tndex: 8
+            },
+            scale: 1,
+            bottomMarginPX: 1700
+        })
+        this.parallaxWrappers.push({
+            leftPercent: 70,
+            parallaxSpeed: 0.5,
+            parallaxItem: {
+                badge: 5,
+                isQEC: true,
+                time: '11:20',
+                title: 'План АСУП',
+                opacity: 0.6,
+                tndex: 9
+            },
+            scale: 1,
+            bottomMarginPX: 1000
+        })
+        this.parallaxWrappers.push({
+            leftPercent: 90,
+            parallaxSpeed: 1,
+            parallaxItem: {
+                badge: 5,
+                isQEC: true,
+                time: '11:20',
+                title: 'План по дебюрократизации',
+                opacity: 1,
+                tndex: 10
+            },
+            scale: 1,
+            bottomMarginPX: 0
+        })
+        this.parallaxWrappers.push({
+            leftPercent: 2,
+            parallaxSpeed: 1.5,
+            parallaxItem: {
+                badge: 5,
+                isQEC: true,
+                time: '11:20',
+                title: 'Отчёт по выполнению гос. заказа',
+                opacity: 1,
+                tndex: 11
+            },
+            scale: 1,
+            bottomMarginPX: 0
+        })
+    }
 
 }
 export class ParallaxWrapper {
-  parallaxItem: PAD77ParallaxItem = {}
-  scale: number = 1
-  bottomMarginPX: number = 0
-  leftPercent: number = 0
-  parallaxSpeed: number = 0
+    parallaxItem: PAD77ParallaxItem = {}
+    scale: number = 1
+    bottomMarginPX: number = 0
+    leftPercent: number = 0
+    parallaxSpeed: number = 0
 }
